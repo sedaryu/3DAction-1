64e14c0b564f655f7d443518dc915c0ab541a5eb
un1207 <120712109+un1207@users.noreply.github.com>
2023-04-20 05:12:51 +0900

    Player 移動・方向転換・アニメ実装
    next => 落下防止

--- Assets/Scenes/FieldScene/Mob/MobController.cs
+++ Assets/Scenes/FieldScene/Mob/MobController.cs
@@ -6,10 +6,13 @@ public abstract class MobController : MonoBehaviour
 {
     //キャラクターの移動はCharacterControllerで行う
     private CharacterController characterController;
+    //アニメーターを格納
+    private Animator animator;
 
     protected void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>(); //アニメーターを取得
     }
 
     // Start is called before the first frame update
@@ -25,8 +28,16 @@ public abstract class MobController : MonoBehaviour
     }
 
     //キャラクターの移動を管理するメソッド
-    protected void Move(Vector3 moving)
+    protected void Move(Vector3 vector)
     {
-        characterController.Move(moving);
+        characterController.Move(vector * 5.5f * Time.deltaTime); //移動入力を更新
+        //キャラクターの向きを更新
+        if (vector != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(vector);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 1000 * Time.deltaTime);
+        }
+        //アニメーターに移動スピードを反映
+        animator.SetFloat("MoveSpeed" , vector.magnitude);
     }
 }
--- Assets/Scenes/FieldScene/Player/PlayerController.cs
+++ Assets/Scenes/FieldScene/Player/PlayerController.cs
@@ -20,10 +20,15 @@ public class PlayerController : MobController
     private void InputMoving()
     {
         Vector3 moving = new Vector3(0, 0, 0);
-        moving.x = Input.GetAxis("Horizontal"); //横方向の移動入力を取得
-        moving.x = Input.GetButton("Horizontal") ? moving.x : 0;
-        moving.z = Input.GetAxis("Vertical"); //縦方向の移動入力を取得
-        moving.z = Input.GetButton("Vertical") ? moving.z : 0;
-        Move(moving.normalized * 5.5f * Time.deltaTime); //入力を反映
+
+        //キー入力
+        moving.x = Input.GetButton("Horizontal") ? Input.GetAxis("Horizontal") : 0; //横方向の移動入力を取得
+        moving.z = Input.GetButton("Vertical") ? Input.GetAxis("Vertical") : 0; //縦方向の移動入力を取得
+
+        //アナログスティック入力
+        moving.x = Input.GetAxis("ASHorizontal") == 0 ? moving.x : Input.GetAxis("ASHorizontal"); //横方向の移動入力を取得
+        moving.z = Input.GetAxis("ASVertical") == 0 ? moving.z : Input.GetAxis("ASVertical"); //縦方向の移動入力を取得
+
+        Move(moving.normalized); //入力を反映
     }
 }
